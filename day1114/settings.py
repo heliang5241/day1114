e157b5402e284420c9ac877449bb23bb53d159f9
heliang5241 <1960050004@qq.com>
2019-11-14 18:43:51 +0800

    add app publisher

--- day1114/settings.py
+++ day1114/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_q*9+tnlz^#xiq)e48%f(#0ba$!44v5-*zg+8z39u)@+*emcxu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'publisher',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'day1114.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'html')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
